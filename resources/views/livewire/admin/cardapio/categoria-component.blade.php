d4da33f9d6461b415658c9fc31a2bf3ad77f27bb
Joaquim Lopes <joaquim@autoconf.com.br>
2020-12-31 17:26:01 -0300

    Fix

--- resources/views/livewire/admin/cardapio/categoria-component.blade.php
+++ resources/views/livewire/admin/cardapio/categoria-component.blade.php
@@ -1,1 +1,53 @@
-<div class="col-md-4" style="height: 500px;">.col-md-4</div>
+<div class="container" style="height: 500px;">
+    <div class="row">
+        <div class="col-6 col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Adicionar uma nova Categoria</h1>
+                </div>
+                <div class="card-body">
+                    
+                    <form>
+                        <div class="form-group row">
+                            <label class="col-sm-3 col-form-label">Address 1</label>
+                            <div class="col-sm-9">
+                              <input type="text" class="form-control">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="col-6 col-sm-6">
+            <div class="card">
+                <div class="card-header">
+                    <h1>Listar de categoria</h1>
+                </div>
+                <div class="card-body">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nome</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2</th>
+                                <td>Jacob</td>
+                            </tr>
+                            <tr>
+                                <th scope="row">3</th>
+                                <td >Larry the Bird</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
